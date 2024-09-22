7bfd9580dc1ab6c75ae7ff91822352f7f3a6b9ba
Marcell Kiss <marcell@kmarcell.com>
2024-09-22 22:58:45 +0200

    Added support for mimetypes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import mimetypes
 import flask
 from flask_cors import CORS
 
 from src.controllers.index_controller import IndexController
 from src.entities.configuration import Configuration
+
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
 
 app = flask.Flask(__name__, template_folder="src/templates")
 configuration = Configuration.create("resources/config.json")
